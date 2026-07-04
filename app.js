728049b90f915cbdb732d0401b2846ad0a7a2b72
Marcin Kapusta <marcinkapusta@Marcins-MacBook-Air.local>
2026-07-04 14:42:26 +0200

    #

---
+++ app.js
@@ -0,0 +1,131 @@
+const options = {
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3OTY0ZmIzMDE4ZDc5ODZjMDZiODJkNWI4YjZkNDg2OSIsIm5iZiI6MTc4MzA3MDMzOC4yMjU5OTk4LCJzdWIiOiI2YTQ3N2U4MjA4ODk0N2EwNzY4YzFiNWIiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.179RYgJUXChVkLSjWNtZnnV7dvdxsHrD8bXQN-UiEBo'
+  }
+};
+
+function discoverTvShows()
+{
+  fetch('https://api.themoviedb.org/3/discover/tv', options)
+  .then(res => res.json())
+  .then(res => {
+    console.log(res);
+        const container = document.querySelector('#container');
+  container.innerHTML = "";
+
+  const listaFilmow = res.results;
+
+  listaFilmow.forEach(serial => {
+      const{name} = serial;
+      const{overview} = serial;
+      const{backdrop_path} = serial;
+      const fullPath = `https://image.tmdb.org/t/p/w500${backdrop_path}`;
+     /* const cardHTML = 
+`
+  <div class="Card">
+    <div class="grid-item foto" style="background-image:url('${fullPath}');"></div>
+    <div class="grid-item text">
+      <h3>${name}</h3>
+      <p>Opis: ${overview}<p/>
+      </div>
+  </div>
+`;*/
+    
+      cardCreate(name, overview, fullPath );
+    });
+  
+  })
+  .catch(err => {
+    console.error(err);
+    container.innerHTML = "Błąd";
+  });
+}
+
+function serchTvShows()
+{
+  fetch('https://api.themoviedb.org/3/search/tv?query=Xplay&language=pl-PL', options)
+  .then(res => res.json())
+  .then(res => {
+    console.log(res);
+        const container = document.querySelector('#container');
+  container.innerHTML = "";
+
+  const listaFilmow = res.results;
+
+  listaFilmow.forEach(serial => {
+      const{name} = serial;
+      const{overview} = serial;
+      const{backdrop_path} = serial;
+      const fullPath = `https://image.tmdb.org/t/p/w500${backdrop_path}`;
+     /* const cardHTML = 
+`
+  <div class="Card">
+    <div class="grid-item foto" style="background-image:url('${fullPath}');"></div>
+    <div class="grid-item text">
+      <h3>${name}</h3>
+      <p>Opis: ${overview}<p/>
+      </div>
+  </div>
+`;*/
+    
+      cardCreate(name, overview, fullPath );
+    });
+  
+  })
+  .catch(err => {
+    console.error(err);
+    container.innerHTML = "Błąd";
+  });
+}
+
+function cardCreate(name, overview, path)
+{
+  let cardHTML = '';
+  console.log("w cardHTML");
+    if(overview != "")
+    {
+      console.log("w if");
+       cardHTML =
+      `<div class="Card">
+        <div class="grid-item">
+          <img src="${path}" alt="${name} Baner">
+        </div>
+        <div class="grid-item text">
+          <p>Opis: ${overview}<p/>
+          </div>
+          <div><h3>${name}</h3></div>
+      </div>
+    `
+  }else{
+    console.log("w else");
+     cardHTML=
+      `<div class="Card">
+        <div class="grid-item">
+          <img src="${path}" alt="${name} Baner">
+        </div>
+          <div><h3>${name}</h3></div>
+      </div>
+    `
+  };
+  container.insertAdjacentHTML('beforeend',cardHTML);
+}
+
+const serchIn = document.querySelector('#serchIn');
+serchIn.addEventListener('input', () => {
+  //console.log("Jest!");
+  const wpisanyTekst = serchIn.value;
+  if(wpisanyTekst == "")
+  {
+    discoverTvShows();
+  }else
+  {
+    serchTvShows();
+  }
+});
+
+
+discoverTvShows();
+
+
